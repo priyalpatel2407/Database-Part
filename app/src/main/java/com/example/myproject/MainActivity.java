c2ce78a24b90a434bf93aac5295fcfdbd615570c
Priyal Patel <priyalpatel@mac.home>
2026-02-01 20:18:12 -0600

    Adding Object Files

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
     public static void main(String[] args){
         System.out.println("hello World\n");
+
+
+
     }
 
 
---
+++ app/src/main/java/com/example/myproject/User.java
@@ -0,0 +1,18 @@
+package com.example.myproject;
+
+public class User {
+
+    private String name ;
+    private String emailAddress ;
+    private int age ;
+
+    public User(String name, String emailAddress ,int age){
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.age = age ;
+    }
+
+
+
+
+}

2104fde3de6a4763a92cdfa25acb01de50f30849
Priyal Patel <priyalpatel@mac.home>
2026-02-03 11:30:26 -0600

    changed the Product

--- app/src/main/java/com/example/myproject/Product.java
+++ app/src/main/java/com/example/myproject/Product.java
@@ -1,7 +1,32 @@
 package com.example.myproject;
 
-import java.util.List;
+public abstract class Product {
+    protected double price ;
+    protected int imgLocation;
 
-public abstract class  Product {
+    protected final long SKU;
+
+    public Product(double price, int imgLocation , long Sku){
+        this.price = price;
+        this.imgLocation = imgLocation;
+        this.SKU = Sku;
+    }
+
+    public long getSKU()  {
+        return this.SKU;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getImgLocation()  {
+        return this.imgLocation;
+    }
+
+
+
+
+
 
 }
